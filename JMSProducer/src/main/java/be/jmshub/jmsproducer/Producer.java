00a914e232e4f8241aca86baaab23a51b310e684
Dimitri Dewaele <dimitri.dewaele@mips.be>
2015-06-04 13:51:45 +0200

    remove target

--- JMSProducer/src/main/java/be/jmshub/jmsproducer/Producer.java
+++ JMSProducer/src/main/java/be/jmshub/jmsproducer/Producer.java
@@ -1,9 +1,92 @@
 package be.jmshub.jmsproducer;
 
+import javax.annotation.Resource;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSRuntimeException;
+import javax.jms.Queue;
+import javax.jms.Topic;
+
 public class Producer {
+
+    @Resource(lookup = "java:comp/DefaultJMSConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    @Resource(lookup = "jms/MyQueue")
+    private static Queue queue;
+    @Resource(lookup = "jms/MyTopic")
+    private static Topic topic;
 
     public static void main(String[] args) {
         System.out.println("JMSProducers main class.");
+        final int NUM_MSGS;
+
+        if ((args.length < 1) || (args.length > 2)) {
+            System.err.println(
+                    "Program takes one or two arguments: "
+                    + "<dest_type> [<number-of-messages>]");
+            System.exit(1);
+        }
+
+        String destType = args[0];
+        System.out.println("Destination type is " + destType);
+
+        if (!(destType.equals("queue") || destType.equals("topic"))) {
+            System.err.println("Argument must be \"queue\" or " + "\"topic\"");
+            System.exit(1);
+        }
+
+        if (args.length == 2) {
+            NUM_MSGS = (new Integer(args[1])).intValue();
+        } else {
+            NUM_MSGS = 1;
+        }
+
+        Destination dest = null;
+
+        try {
+            if (destType.equals("queue")) {
+                dest = (Destination) queue;
+            } else {
+                dest = (Destination) topic;
+            }
+        } catch (JMSRuntimeException e) {
+            System.err.println("Error setting destination: " + e.toString());
+            System.exit(1);
+        }
+
+        /*
+         * Within a try-with-resources block, create context.
+         * Create producer and message.
+         * Send messages, varying text slightly.
+         * Send end-of-messages message.
+         */
+        try (JMSContext context = connectionFactory.createContext();) {
+            int count = 0;
+
+            for (int i = 0; i < NUM_MSGS; i++) {
+                String message = "This is message " + (i + 1) 
+                        + " from producer";
+                // Comment out the following line to send many messages
+                System.out.println("Sending message: " + message);
+                context.createProducer().send(dest, message);
+                count += 1;
+            }
+            System.out.println("Text messages sent: " + count);
+            
+            /*
+             * Send a non-text control message indicating end of
+             * messages.
+             */
+            context.createProducer().send(dest, context.createMessage());
+            // Uncomment the following line if you are sending many messages
+            // to two synchronous consumers
+            // context.createProducer().send(dest, context.createMessage());
+        } catch (JMSRuntimeException e) {
+            System.err.println("Exception occurred: " + e.toString());
+            System.exit(1);
+        }
+        System.exit(0);
     }
-    
+
 }
