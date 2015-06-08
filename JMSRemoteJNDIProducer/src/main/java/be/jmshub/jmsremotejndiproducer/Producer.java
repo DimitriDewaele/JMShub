e55a657758984a1ad5474de314f9a0531a4002ac
Dimitri Dewaele <dimitri.dewaele@mips.be>
2015-06-08 15:16:01 +0200

    produce

---
+++ JMSRemoteJNDIProducer/src/main/java/be/jmshub/jmsremotejndiproducer/Producer.java
@@ -0,0 +1,68 @@
+package be.jmshub.jmsremotejndiproducer;
+
+import javax.jms.JMSException;
+import com.sun.messaging.ConnectionConfiguration;
+import com.sun.messaging.ConnectionFactory;
+import com.sun.messaging.Queue;
+import java.util.Properties;
+import javax.jms.Connection;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.Message;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class Producer {
+
+    public static void main(String[] args) throws JMSException {
+        System.out.println("JMS REMOTE PRODUCER: start");
+        Queue queue = new Queue("MyQueue");
+        try {
+            Properties env = new Properties();
+            
+            //FOR GLASSFISH
+            env.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+            env.setProperty(Context.URL_PKG_PREFIXES, "com.sun.enterprise.naming");
+            env.setProperty(Context.STATE_FACTORIES, "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+//            env.setProperty(Context.PROVIDER_URL, "iiop://localhost:7676");
+            env.put("org.omg.CORBA.ORBInitialHost", "localhost");
+            env.put("org.omg.CORBA.ORBInitialPort", "7676");
+            
+//            //FOR JBOSS
+//            env.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+//            env.setProperty(Context.PROVIDER_URL, "remote://localhost:4447");
+
+            InitialContext initialContext = new InitialContext(env);
+            ConnectionFactory connectionFactory = null;
+            try {
+                connectionFactory = (ConnectionFactory) initialContext.lookup("jms/DefaultJMSConnectionFactory");
+
+                Connection connection = connectionFactory.createConnection();
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                MessageProducer producer = session.createProducer(queue);
+
+                System.out.println("JMS REMOTE PRODUCER: connected");
+
+                int count = 0;
+
+                for (int i = 0; i < 5; i++) {
+                    String messageText = "This is message " + (i + 1);
+                    Message message = session.createTextMessage(messageText);
+                    System.out.println("JMS REMOTE PRODUCER: sending - " + messageText);
+                    producer.send(message);
+                    count += 1;
+                }
+                System.out.println("JMS REMOTE PRODUCER: total messages sent: " + count);
+
+            } catch (Exception e) {
+                System.out.println("JNDI API lookup failed: " + e.toString());
+                e.printStackTrace();
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+
+        System.out.println("JMS REMOTE PRODUCER: finished");
+    }
+}
--- JMSRemoteProducer/src/main/java/be/jmshub/jmsremoteproducer/Producer.java
+++ JMSRemoteProducer/src/main/java/be/jmshub/jmsremoteproducer/Producer.java
@@ -4,7 +4,6 @@ import javax.jms.JMSException;
 import com.sun.messaging.ConnectionConfiguration;
 import com.sun.messaging.ConnectionFactory;
 import com.sun.messaging.Queue;
-import com.sun.messaging.Topic;
 import javax.jms.Connection;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
@@ -12,97 +11,37 @@ import javax.jms.Message;
 
 public class Producer {
 
-//    @Resource(lookup = "java:comp/DefaultJMSConnectionFactory")
     private static ConnectionFactory connectionFactory;
-//    @Resource(lookup = "jms/MyQueue")
     private static Queue queue;
-//    @Resource(lookup = "jms/MyTopic")
-    private static Topic topic;
-
+    
     public static void main(String[] args) throws JMSException {
-        System.out.println("JMSProducers main class.");
+        System.out.println("JMS REMOTE PRODUCER: start");
+
+        queue = new Queue("MyQueue");
 
         connectionFactory = new ConnectionFactory();
         connectionFactory.setProperty(ConnectionConfiguration.imqAddressList, "localhost:7676");
 
-        queue = new Queue("MyQueue");
-        topic = new Topic("MyTopic");
+        try {
+            Connection connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            MessageProducer producer = session.createProducer(queue);
 
-        try (Connection connection = connectionFactory.createConnection();
-                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                MessageProducer producer = session.createProducer(queue)) {
+            System.out.println("JMS REMOTE PRODUCER: connected");
 
-            Message message = session.createTextMessage("this is my test message");
-            producer.send(message);
+            int count = 0;
+
+            for (int i = 0; i < 5; i++) {
+                String messageText = "This is message " + (i + 1);
+                Message message = session.createTextMessage(messageText);
+                System.out.println("JMS REMOTE PRODUCER: sending - " + messageText);
+                producer.send(message);
+                count += 1;
+            }
+            System.out.println("JMS REMOTE PRODUCER: total messages sent: " + count);
+        } catch (Exception ex) {
+            System.out.println("JMS REMOTE PRODUCER: EXCEPTION");
         }
-        System.out.println("Message away");
-
-//        final int NUM_MSGS;
-//
-//        if ((args.length < 1) || (args.length > 2)) {
-//            System.err.println("Program takes one or two arguments: <dest_type> [<number-of-messages>]");
-//            System.exit(1);
-//        }
-//
-//        String destType = args[0];
-//        System.out.println("Destination type is " + destType);
-//
-//        if (!(destType.equals("queue") || destType.equals("topic"))) {
-//            System.err.println("Argument must be \"queue\" or " + "\"topic\"");
-//            System.exit(1);
-//        }
-//
-//        if (args.length == 2) {
-//            NUM_MSGS = (new Integer(args[1])).intValue();
-//        } else {
-//            NUM_MSGS = 1;
-//        }
-//
-//        Destination dest = null;
-//
-//        try {
-//            if (destType.equals("queue")) {
-//                dest = (Destination) queue;
-//            } else {
-//                dest = (Destination) topic;
-//            }
-//        } catch (JMSRuntimeException e) {
-//            System.err.println("Error setting destination: " + e.toString());
-//            System.exit(1);
-//        }
-//
-//        /*
-//         * Within a try-with-resources block, create context.
-//         * Create producer and message.
-//         * Send messages, varying text slightly.
-//         * Send end-of-messages message.
-//         */
-//        try (JMSContext context = connectionFactory.createContext();) {
-//            int count = 0;
-//
-//            for (int i = 0; i < NUM_MSGS; i++) {
-//                String message = "This is message " + (i + 1)
-//                        + " from producer";
-//                // Comment out the following line to send many messages
-//                System.out.println("Sending message: " + message);
-//                context.createProducer().send(dest, message);
-//                count += 1;
-//            }
-//            System.out.println("Text messages sent: " + count);
-//
-//            /*
-//             * Send a non-text control message indicating end of
-//             * messages.
-//             */
-//            context.createProducer().send(dest, context.createMessage());
-//            // Uncomment the following line if you are sending many messages
-//            // to two synchronous consumers
-//            // context.createProducer().send(dest, context.createMessage());
-//        } catch (JMSRuntimeException e) {
-//            System.err.println("Exception occurred: " + e.toString());
-//            System.exit(1);
-//        }
-//        System.exit(0);
+        System.out.println("JMS REMOTE PRODUCER: finished");
     }
-
 }
